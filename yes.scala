dd40864c64774fa0c7cea5d61251e54fbd592b03
Huu Nguyen <whoshuu@gmail.com>
2016-03-04 12:11:46 -0800

    Handle pipe signal in yes.scala

--- yes.scala
+++ yes.scala
@@ -1,7 +1,19 @@
+import sun.misc.Signal
+import sun.misc.SignalHandler
+
 object yes
 {
     def main(args: Array[String])
     {
+        val signal = new Signal("PIPE");
+        val handler = new SignalHandler {
+            def handle(signal: Signal): Unit = {
+                sys.exit(13)
+            }
+        }
+
+        Signal.handle(signal, handler)
+
         if (args.length == 0)
         {
             while(true)
